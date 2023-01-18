6f41252dce8a117c4dc3485fe0febe37c1666496
Shreya Pandey <74672603+shreya5402@users.noreply.github.com>
2023-01-18 21:59:17 +0530

    Recursive Approach

--- MergeTwoSortedLists.cpp
+++ MergeTwoSortedLists.cpp
@@ -1,7 +1,6 @@
 //Inplace is when you do not create external nodes.
 //Time complexity:- O(m+n)
 //here we maintain a head and a tail pointer on the merged linked list and choose the head of the merged list by comparing the first node of both the linked lists.For all subsequent nodes in both lists we choose the smaller current node and link it to the tail of the merged list while moving the current pointer of that list one step forward.
-					// 😉😉😉😉Please upvote if it helps 😉😉😉😉
 class Solution {
 public:
 	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
@@ -14,8 +13,8 @@ public:
         {
 			return l1;
 		} 	
-		// if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
-		// we wall call recursively l1 -> next and whole l2 list.
+		//if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
+		//we wall call recursively l1 -> next and whole l2 list.
 		if(l1 -> val <= l2 -> val)
         {
 			l1 -> next = mergeTwoLists(l1 -> next, l2);
