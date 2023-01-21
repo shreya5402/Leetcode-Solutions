3a33d25773ea2cf4ec1ba2bfb4bf520b5c7a0fa3
Shreya Pandey <74672603+shreya5402@users.noreply.github.com>
2023-01-21 13:27:04 +0530

    .

--- 1.TwoSums.cpp
+++ 1.TwoSums.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> ans;
         unordered_map<int, int> mpp;
         for(int i=0; i<nums.size(); i++){
-            if(mpp.find(target-nums[i])!=mpp.end()){
+            if(mpp.find(target-nums[i]) != mpp.end()){
                 ans.push_back(mpp[target-nums[i]]);
                 ans.push_back(i);
                 return ans;
