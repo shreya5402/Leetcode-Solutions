0afe24d31e07059ab40bedd28bbce53d0b60ba40
Shreya Pandey <74672603+shreya5402@users.noreply.github.com>
2023-01-22 11:54:45 +0530

    .

--- LengthOfLastWord.py
+++ LengthOfLastWord.py
@@ -1,3 +1,11 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         return len(s.split()[-1])
+    
+    
+#Another way of doing it:-
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.strip().split()[-1])
+    
+#s.strip() is used to remove the whitespaces from the string and split() is used to turn the string into a list where each word will be a list item.
